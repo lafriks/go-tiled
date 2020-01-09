7e99b4b4d60ea63d430e45ecc14f0e8fb7e85a0c
Tim Cooper <tim.cooper@layeh.com>
2020-01-09 11:29:17 -0600

    add imagelayer support
    Fixes #11

--- tmx_image.go
+++ tmx_image.go
@@ -22,6 +22,54 @@ SOFTWARE.
 
 package tiled
 
+import (
+	"encoding/xml"
+)
+
+// ImageLayer is a layer consisting of a single image.
+type ImageLayer struct {
+	// Unique ID of the layer.
+	// Each layer that added to a map gets a unique id. Even if a layer is deleted,
+	// no layer ever gets the same ID. Can not be changed in Tiled. (since Tiled 1.2)
+	ID uint32 `xml:"id,attr"`
+	// The name of the image layer.
+	Name string `xml:"name,attr"`
+	// Rendering offset of the image layer in pixels. Defaults to 0. (since 0.15)
+	OffsetX int `xml:"offsetx,attr"`
+	// Rendering offset of the image layer in pixels. Defaults to 0. (since 0.15)
+	OffsetY int `xml:"offsety,attr"`
+	// The x position of the image layer in pixels. (deprecated since 0.15)
+	X int `xml:"x,attr"`
+	// The y position of the image layer in pixels. (deprecated since 0.15)
+	Y int `xml:"y,attr"`
+	// The opacity of the layer as a value from 0 to 1. Defaults to 1.
+	Opacity float32 `xml:"opacity,attr"`
+	// Whether the layer is shown (1) or hidden (0). Defaults to 1.
+	Visible bool `xml:"visible,attr"`
+	// Custom properties
+	Properties Properties `xml:"properties>property"`
+	// The group image
+	Image *Image `xml:"image"`
+}
+
+// UnmarshalXML decodes a single XML element beginning with the given start element.
+func (l *ImageLayer) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	type Alias ImageLayer
+
+	item := Alias{
+		Opacity: 1,
+		Visible: true,
+	}
+
+	if err := d.DecodeElement(&item, &start); err != nil {
+		return err
+	}
+
+	*l = (ImageLayer)(item)
+
+	return nil
+}
+
 // Image source
 type Image struct {
 	// Used for embedded images, in combination with a data child element. Valid values are file extensions like png, gif, jpg, bmp, etc.
